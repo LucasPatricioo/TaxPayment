c859b6634d1192f8da9098595187c5bf7ab36d13
Lucas Patricio <lucas_patricio@outlook.com.br>
2020-07-27 19:02:50 -0300

    Adicionando subclasses Individual e Company ao projeto.

---
+++ TaxPayment/Entities/Individual.cs
@@ -0,0 +1,32 @@
+﻿namespace TaxPayment.Entities
+{
+    class Individual : TaxPayer
+    {
+        public double HealthExpenditures { get; private set; }
+        public Individual(string name, double anualIncome, double healthExpenditures) : base(name, anualIncome)
+        {
+            HealthExpenditures = healthExpenditures;
+        }
+        public override double Tax()
+        {
+            double inc = 0;
+            if (AnualIncome < 20000)
+            {
+                inc = AnualIncome * 0.15;
+            }
+            else
+            {
+                inc = AnualIncome * 0.25;
+            }
+            if (HealthExpenditures != 0)
+            {
+                return inc - (HealthExpenditures * 0.50);
+            }
+            else
+            {
+                return inc;
+            }
+        }
+
+    }
+}
