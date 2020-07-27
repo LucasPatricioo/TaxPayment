1fd52e593513bdafc171f3e15f43d7508dd58030
Lucas Patricio <lucas_patricio@outlook.com.br>
2020-07-27 18:45:22 -0300

    Adicionando classe TaxPayer

---
+++ TaxPayment/Entities/TaxPayer.cs
@@ -0,0 +1,15 @@
+﻿namespace TaxPayment.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; private set; }
+        public double AnualIncome { get; protected set; }
+
+        public TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+        public abstract double Tax();
+    }
+}
